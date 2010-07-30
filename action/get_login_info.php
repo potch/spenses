ec6a306aae0b9d41a96df476ba54f900b389fd90
Andrew Pariser <pariser@gmail.com>
2010-07-29 22:13:53 -0700

    More openid workings

--- action/get_login_info.php
+++ action/get_login_info.php
@@ -24,6 +24,8 @@ try {
   $openid = new LightOpenID;
 
   $openid->identity = $data["openid"];
+  $openid->realm = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
+  $openid->openid->returnUrl = $openid->realm . 'index.php';
 
   $data["url"] = $openid->authUrl();
 
--- user.php
+++ user.php
@@ -1,4 +1,20 @@
 <?php
-  $needslogin = false;
-  if (!isset($_COOKIE) || !isset($_COOKIE['user'])) $needslogin = true;    
+
+require "lib/openid.php";
+
+$needslogin = false;
+if (!isset($_COOKIE) || !isset($_COOKIE['user'])) $needslogin = true;    
+
+if ($needslogin && isset($_GET['openid_mode'])) {
+
+    if ($_GET['openid_mode'] == 'cancel') {
+    } else {
+      $openid = new LightOpenID;
+      $valid = $openid->validate();
+
+      if ($valid)
+	header('Location: action/dologin.php');
+    }
+}
+
 ?>
